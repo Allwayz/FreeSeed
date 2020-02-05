ca35e729b2baa840e1ebd0afb4182104fe23ec16
allwayz <2584491610@qq.com>
2020-02-05 08:07:04 +0800

    Inital commit

---
+++ src/main/java/com/allwayz/freeseed/FreeseedApplication.java
@@ -0,0 +1,11 @@
+package com.allwayz.freeseed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeseedApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FreeseedApplication.class,args);
+    }
+}
