543c886d34b458ad444e7147793d1d979e14eb70
allwayz <2584491610@qq.com>
2020-02-12 15:01:52 +0800

    Inital commit

--- src/main/java/com/allwayz/freeseed/util/MailUtil.java
+++ src/main/java/com/allwayz/freeseed/util/MailUtil.java
@@ -7,13 +7,9 @@ import jetbrick.template.JetTemplate;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.web.multipart.support.StandardMultipartHttpServletRequest;
 
-import javax.security.auth.login.Configuration;
 import java.io.File;
-import java.io.IOException;
 import java.io.StringWriter;
-import java.io.Writer;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
@@ -23,7 +19,7 @@ import static io.github.biezhi.ome.OhMyEmail.SMTP_QQ;
 
 public class MailUtil {
     // 该邮箱修改为你需要测试的邮箱地址
-    private static final String TO_EMAIL = "allwayzio@126.com";
+    private static final String TO_EMAIL = "374615181@qq.com";
 
 
     @Before
@@ -32,6 +28,61 @@ public class MailUtil {
         OhMyEmail.config(SMTP_QQ(false), "2584491610@qq.com", "bbtmcybrvxbddhic");
         // 如果是企业邮箱则使用下面配置
         //OhMyEmail.config(SMTP_ENT_QQ(false), "xxx@qq.com", "*******");
+    }
+
+    /**
+     *
+     * @param email
+     * @param user
+     * @throws SendMailException
+     */
+    public static void sendWelcomeEmail(String email, String user) throws SendMailException {
+        JetEngine engine = JetEngine.create();
+        JetTemplate template = engine.getTemplate("/templates/JetxTemplates/Welcome.jetx");
+
+        Map<String, Object> context = new HashMap<String, Object>();
+        context.put("username", user);
+
+        StringWriter writer = new StringWriter();
+        template.render(context, writer);
+        String output = writer.toString();
+        System.out.println(output);
+
+        OhMyEmail.subject("Welcome to FreeSeed")
+                .from("Allwayz")
+                .to(email)
+                .html(output)
+                .send();
+        Assert.assertTrue(true);
+        System.out.println("Send Success...");
+    }
+
+    /**
+     *
+     * @param email
+     * @throws SendMailException
+     */
+    public static void sendAuthorizationCodeEmail(String email) throws SendMailException {
+        JetEngine engine = JetEngine.create();
+        JetTemplate template = engine.getTemplate("/templates/JetxTemplates/AuthorizationCode.jetx");
+
+        String code = AuthorizationCodeUtil.randomCode();
+        Map<String, Object> context = new HashMap<String, Object>();
+        context.put("code", code);
+
+        StringWriter writer = new StringWriter();
+        template.render(context, writer);
+        String output = writer.toString();
+        System.out.println(output);
+
+        OhMyEmail.subject("FreeSeed Authorization")
+                .from("Allwayz")
+                .to(email)
+                .html(output)
+                .send();
+        Assert.assertTrue(true);
+        System.out.println(code);
+        System.out.println("Send...");
     }
 
     @Test
@@ -124,4 +175,10 @@ public class MailUtil {
         System.out.println("Send...");
     }
 
+    @Test
+    public void testWelcome() throws SendMailException{
+        String email = "allwayzio@126.com";
+        String username = "Allwayz";
+        MailUtil.sendWelcomeEmail(email,username);
+    }
 }
